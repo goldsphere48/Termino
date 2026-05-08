06e23ffa9605250c752f8476b1511f7d7d694bf0
Viktor Titov <goldsphere48@gmail.com>
2026-05-09 00:32:56 +1000

    Clean folder structure

--- compiler/build.bat
+++ compiler/build.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 
-set SOURCE=main.cpp lexer.cpp error.cpp isa_helper.cpp parser.cpp parser_helper.cpp
+set SOURCE=src/main.cpp src/lexer.cpp src/error.cpp src/isa_helper.cpp src/parser.cpp src/parser_helper.cpp
 set BUILD_DIR=build
 set OUT=compiler.exe
 
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 
-clang %SOURCE% -o %BUILD_DIR%\%OUT% -O0 -g -std=c++20
+clang %SOURCE% -o %BUILD_DIR%\%OUT% -O0 -g -std=c++20 -Iinclude
